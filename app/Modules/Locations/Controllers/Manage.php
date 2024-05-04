7dc0ac3aaaa4530b6dcc28b145481ac390e8d261
Đạt <>
2024-05-04 10:38:56 +0700

    fix loacation

--- app/Modules/Locations/Controllers/Manage.php
+++ app/Modules/Locations/Controllers/Manage.php
@@ -130,6 +130,8 @@ class Manage extends AdminController
             $json['success'] = lang('Admin.text_edit_success');
         }
 
+        $this->model->deleteCache();
+
         json_output($json);
     }
 
@@ -199,6 +201,7 @@ class Manage extends AdminController
             }
 
             $this->model->delete($ids);
+            $this->model->deleteCache();
 
             json_output(['token' => $token, 'status' => 'ok', 'ids' => $ids, 'msg' => lang('Admin.text_delete_success')]);
         }
