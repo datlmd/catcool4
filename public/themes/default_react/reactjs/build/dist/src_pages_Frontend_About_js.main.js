3a94523488f649e50a738ba5e0cdd3e918dbbd93
datlmd <lmd.dat@gmail.com>
2024-10-23 22:46:05 +0700

    add react build

---
+++ public/themes/default_react/reactjs/build/dist/src_pages_Frontend_About_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkreactjs"] = self["webpackChunkreactjs"] || []).push([["src_pages_Frontend_About_js"],{
+
+/***/ "./src/pages/Frontend/About.js":
+/*!*************************************!*\
+  !*** ./src/pages/Frontend/About.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst PageAbout = () => {\n  return /*#__PURE__*/React.createElement(\"h1\", null, \"About US\");\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PageAbout);\n\n//# sourceURL=webpack://reactjs/./src/pages/Frontend/About.js?");
+
+/***/ })
+
+}]);
---
+++ public/themes/default_react/reactjs/build/dist/src_pages_Frontend_NotFound_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkreactjs"] = self["webpackChunkreactjs"] || []).push([["src_pages_Frontend_NotFound_js"],{
+
+/***/ "./src/pages/Frontend/NotFound.js":
+/*!****************************************!*\
+  !*** ./src/pages/Frontend/NotFound.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst PageNotFound = () => {\n  return /*#__PURE__*/React.createElement(\"h1\", null, \"Not\");\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PageNotFound);\n\n//# sourceURL=webpack://reactjs/./src/pages/Frontend/NotFound.js?");
+
+/***/ })
+
+}]);
