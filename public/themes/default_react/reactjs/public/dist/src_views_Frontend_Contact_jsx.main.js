de5198f067b78f65c2b2b9bbabb697720081dd0f
Dat Le <>
2024-11-02 22:54:53 +0700

    build dev react

--- public/themes/default_react/reactjs/public/dist/src_views_Frontend_Contact_jsx.main.js
+++ public/themes/default_react/reactjs/public/dist/src_views_Frontend_Contact_jsx.main.js
@@ -7,7 +7,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(self["webpackChunkreactjs"] = self["webpackChunkreactjs"] || []).push([["src_pages_Frontend_Contact_jsx"],{
+(self["webpackChunkreactjs"] = self["webpackChunkreactjs"] || []).push([["src_views_Frontend_Contact_jsx"],{
 
 /***/ "./src/components/Contact/Form.jsx":
 /*!*****************************************!*\
@@ -29,16 +29,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
-/***/ "./src/pages/Frontend/Contact.jsx":
-/*!****************************************!*\
-  !*** ./src/pages/Frontend/Contact.jsx ***!
-  \****************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _utils_callApi__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../utils/callApi */ \"./src/utils/callApi.js\");\n/* harmony import */ var _components_Contact_Form_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../components/Contact/Form.jsx */ \"./src/components/Contact/Form.jsx\");\n/* harmony import */ var _components_Loading_Content_jsx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../components/Loading/Content.jsx */ \"./src/components/Loading/Content.jsx\");\n\n\n\n\n\n\nconst PageContact = props => {\n  const [isLoading, setIsLoading] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(true);\n  const [contents, setContents] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]);\n  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {\n    //const [data, triggerData] = useCache(\"key_page_contact\", LoadPage());\n    LoadPage();\n  }, []);\n  const sendLayout = layouts => {\n    props.parentLayout(layouts);\n  };\n  const LoadPage = async () => {\n    try {\n      const response = await _utils_callApi__WEBPACK_IMPORTED_MODULE_1__.API.get(\"frontend/api/contact\", (0,_utils_callApi__WEBPACK_IMPORTED_MODULE_1__.getRequestConfiguration)());\n      setContents(response.data);\n      setIsLoading(false);\n      sendLayout(response.data.layouts);\n    } catch (error) {\n      console.error(error);\n    }\n  };\n  return isLoading ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_Loading_Content_jsx__WEBPACK_IMPORTED_MODULE_3__[\"default\"], null) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, contents.lang.contact), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_Contact_Form_jsx__WEBPACK_IMPORTED_MODULE_2__[\"default\"], {\n    contents: true\n  }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PageContact);\n\n//# sourceURL=webpack://reactjs/./src/pages/Frontend/Contact.jsx?");
-
-/***/ }),
-
 /***/ "./src/utils/callApi.js":
 /*!******************************!*\
   !*** ./src/utils/callApi.js ***!
@@ -46,6 +36,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   API: () => (/* binding */ API),\n/* harmony export */   getRequestConfiguration: () => (/* binding */ getRequestConfiguration),\n/* harmony export */   makeRequest: () => (/* binding */ makeRequest)\n/* harmony export */ });\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ \"./node_modules/axios/lib/axios.js\");\n\nconst API = axios__WEBPACK_IMPORTED_MODULE_0__[\"default\"].create({\n  baseURL: window.base_url,\n  responseType: 'json',\n  contentType: 'application/json',\n  headers: {\n    \"X-Requested-With\": \"XMLHttpRequest\"\n  }\n});\nconst getRequestConfiguration = authorization => {\n  const headers = {\n    'Content-Type': 'application/json'\n  };\n  if (authorization) headers.Authorization = `Bearer ${authorization}`;\n  return {\n    headers\n  };\n};\nconst makeRequest = ({\n  url,\n  values,\n  successCallback,\n  failureCallback,\n  requestType = 'POST',\n  authorization = null\n}) => {\n  const requestConfiguration = getRequestConfiguration(authorization);\n  let promise;\n  switch (requestType) {\n    case 'GET':\n      promise = API.get(url, requestConfiguration);\n      break;\n    case 'POST':\n      promise = API.post(url, values, requestConfiguration);\n      break;\n    case 'DELETE':\n      promise = API.delete(url, requestConfiguration);\n      break;\n    default:\n      return;\n  }\n  promise.then(response => {\n    const {\n      data\n    } = response;\n    successCallback(data);\n  }).catch(error => {\n    if (error.response) {\n      failureCallback(error.response.data);\n    }\n  });\n};\n\n//# sourceURL=webpack://reactjs/./src/utils/callApi.js?");
+
+/***/ }),
+
+/***/ "./src/views/Frontend/Contact.jsx":
+/*!****************************************!*\
+  !*** ./src/views/Frontend/Contact.jsx ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _utils_callApi__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../utils/callApi */ \"./src/utils/callApi.js\");\n/* harmony import */ var _components_Contact_Form_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../components/Contact/Form.jsx */ \"./src/components/Contact/Form.jsx\");\n/* harmony import */ var _components_Loading_Content_jsx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../components/Loading/Content.jsx */ \"./src/components/Loading/Content.jsx\");\n\n\n\n\n\n\nconst PageContact = props => {\n  const [isLoading, setIsLoading] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(true);\n  const [contents, setContents] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]);\n  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {\n    //const [data, triggerData] = useCache(\"key_page_contact\", LoadPage());\n    LoadPage();\n  }, []);\n  const sendLayout = layouts => {\n    props.parentLayout(layouts);\n  };\n  const LoadPage = async () => {\n    try {\n      const response = await _utils_callApi__WEBPACK_IMPORTED_MODULE_1__.API.get(\"frontend/api/contact\", (0,_utils_callApi__WEBPACK_IMPORTED_MODULE_1__.getRequestConfiguration)());\n      setContents(response.data);\n      setIsLoading(false);\n      sendLayout(response.data.layouts);\n    } catch (error) {\n      console.error(error);\n    }\n  };\n  return isLoading ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_Loading_Content_jsx__WEBPACK_IMPORTED_MODULE_3__[\"default\"], null) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, contents.lang.contact), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_Contact_Form_jsx__WEBPACK_IMPORTED_MODULE_2__[\"default\"], {\n    contents: true\n  }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PageContact);\n\n//# sourceURL=webpack://reactjs/./src/views/Frontend/Contact.jsx?");
 
 /***/ })
 
---
+++ public/themes/default_react/reactjs/public/dist/src_views_Frontend_NotFound_jsx.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkreactjs"] = self["webpackChunkreactjs"] || []).push([["src_views_Frontend_NotFound_jsx"],{
+
+/***/ "./src/views/Frontend/NotFound.jsx":
+/*!*****************************************!*\
+  !*** ./src/views/Frontend/NotFound.jsx ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ PageNotFound)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ \"./node_modules/react-router-dom/dist/index.js\");\n\n\nfunction PageNotFound() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"wrap\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, \"404 - Page Not Found\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", null, \"Could not find the page you were looking for.\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.Link, {\n    to: '/'\n  }, \"Return to Dashboard\")));\n}\n\n//# sourceURL=webpack://reactjs/./src/views/Frontend/NotFound.jsx?");
+
+/***/ })
+
+}]);
