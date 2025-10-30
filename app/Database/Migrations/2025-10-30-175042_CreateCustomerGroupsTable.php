ada2e51131467ecbe26eae35a9bbbf7620a46c21
datlmd <lmd.dat@gmail.com>
2025-10-30 22:30:58 +0700

    migrate customer group & approval

---
+++ app/Database/Migrations/2025-10-30-175042_CreateCustomerGroupsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCustomerGroupsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'customer_group_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT',
+            'approval INT(1) NOT NULL',
+            'sort_order INT(3) NULL',
+        ]);
+
+        // Khóa chính kép
+        $this->forge->addKey('customer_group_id', true);
+
+        // Tạo bảng
+        $this->forge->createTable('customer_groups');
+
+        /**
+         * Tạo table: customer_group_translations
+         */
+        $this->forge->addField([
+            'customer_group_id BIGINT UNSIGNED NOT NULL',
+            'language_id INT(11) NOT NULL',
+            'name VARCHAR(32) NOT NULL',
+            'description TEXT NULL',
+        ]);
+
+        // Khóa chính kép
+        $this->forge->addKey(['customer_group_id', 'language_id'], true);
+
+        $this->forge->addForeignKey(
+            'customer_group_id',
+            'customer_groups',
+            'customer_group_id',
+            'CASCADE',
+            'CASCADE',
+            'fk_customer_group_translations_customer_group_id'
+        );
+
+        // Tạo bảng
+        $this->forge->createTable('customer_group_translations');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('customer_group_translations');
+        $this->forge->dropTable('customer_groups');
+    }
+}
---
+++ app/Database/Migrations/2025-10-30-175044_CreateCustomerApprovalsTable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCustomerApprovalsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'customer_approval_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT',
+            'customer_id BIGINT UNSIGNED NOT NULL',
+            'type VARCHAR(9) NOT NULL',
+            'created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
+        ]);
+
+        // Khóa chính
+        $this->forge->addKey('customer_approval_id', true);
+
+        // Tạo bảng
+        $this->forge->createTable('customer_approvals');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('customer_approvals');
+    }
+}
